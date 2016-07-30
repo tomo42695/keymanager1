72f1cd38e3bd80fa0018223a47252cc8e40766ca
岩井　智夢 <tomo42695@gmail.com>
2016-07-30 15:37:03 +0900

    編集・削除機能実装

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -11,15 +11,32 @@ class RentalsController < ApplicationController
     rental = Rental.create(create_params)
   end
 
-    def destroy
-      rental = Rental.find(params[:id])
-      if rental.user_id == current_user.id
-        rental.destroy
-      end
+  def destroy
+    rental = Rental.find(params[:id])
+    if rental.user_id == current_user.id
+      rental.destroy
     end
+  end
+
+  def edit
+    @rental = Rental.find(params[:id])
+  end
+
+  def update
+    rental = Rental.find(params[:id])
+    if rental.user_id == current_user.id
+      rental.update(rental_params)
+    end
+  end
+
 
   private
   def create_params
     params.require(:rental).permit(:room_id, :door_state, :rent_time, :return_time, :groupname).merge(user_id: current_user.id)
+  end
+
+  private
+  def rental_params
+    params.require(:rental).permit(:room_id, :door_state, :rent_time, :return_time, :groupname)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'rentals#index'
   # resources :rentals
-  resources :rentals, only: [:index, :new, :create, :destroy]
+  resources :rentals, except: [:show]
 end
